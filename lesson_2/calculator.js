2d5955039ce481ca2a97c20210bf8effdea76e93
Will Conrad <w.conrad@outlook.com>
2022-09-14 14:55:36 -0500

    Modified Calculator and json

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -3,62 +3,84 @@
 // Ask the user for an operation to perform.
 // Perform the operation on the two numbers.
 // Print the result to the terminal.
+const MESSAGES = require('./calculator_messages.json');
 
 const readline = require('readline-sync');
+let answer;
+const langaugeCheck = Object.keys(MESSAGES);
+
 
 function prompt(msg) {
   console.log(`=> ${msg}`);
-}
-
-prompt('Welcome to Calculator');
-
-prompt('What is your first number?');
-let number1 = readline.question();
-
-while (invalidNumber(number1)) {
-  prompt("Hmm... that doesn't look like a valud number.");
-  number1 = readline.question();
 }
 
 function invalidNumber(number) {
   return number.trimStart() === '' || Number.isNaN(Number(number));
 }
 
-prompt('What is your second number?');
-let number2 = readline.question();
+for (let i = 0; i < langaugeCheck.length; i += 1) {
+  prompt(MESSAGES[langaugeCheck[i]].welcomeMessage);
+}
+let language = readline.question().toLowerCase();
 
-while (invalidNumber(number2)) {
-  prompt("Hmm... that doesn't look like a valud number.");
-  number2 = readline.question();
+
+while (!langaugeCheck.includes(language)) {
+  for (let i = 0; i < langaugeCheck.length; i += 1) {
+    prompt(MESSAGES[langaugeCheck[i]].invalidLanguage);
+  }
+  language = readline.question();
 }
 
+let languageMessages = MESSAGES[language];
 
-//console.log(`${number1} ${number2}`);
+do {
 
-prompt('What operation would you like to preform\n1) Add 2) Subtract 3) Mutiply 4) Divide');
-let operation = readline.question();
+  prompt(languageMessages.firstNumber);
+  let number1 = readline.question();
 
-while (!['1', '2', '3', '4'].includes(operation)) {
-  prompt('Must choose 1,2,3,4');
-  operation = readline.question();
-}
+  while (invalidNumber(number1)) {
+    prompt(languageMessages.invalidNumber);
+    number1 = readline.question();
+  }
 
-let output;
+  prompt(languageMessages.secondNumber);
+  let number2 = readline.question();
 
-switch (operation) {
-  case '1':
-    output = Number(number1) + Number(number2);
-    break;
-  case '2':
-    output = Number(number1) - Number(number2);
-    break;
-  case '3':
-    output = Number(number1) * Number(number2);
-    break;
-  case '4':
-    output = Number(number1) / Number(number2);
-    break;
-}
+  while (invalidNumber(number2)) {
+    prompt(languageMessages.invalidNumber);
+    number2 = readline.question();
+  }
 
-prompt(`The result is: ${output}`);
+  prompt(languageMessages.operationPrompt);
+  let operation = readline.question();
 
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    prompt(languageMessages.invalidOperation);
+    operation = readline.question();
+  }
+
+  let output;
+
+  switch (operation) {
+    case '1':
+      output = Number(number1) + Number(number2);
+      break;
+    case '2':
+      output = Number(number1) - Number(number2);
+      break;
+    case '3':
+      output = Number(number1) * Number(number2);
+      break;
+    case '4':
+      output = Number(number1) / Number(number2);
+      break;
+  }
+
+  prompt(`The result is: ${output}`);
+
+  prompt(languageMessages.anotherCalcualtion);
+  answer = readline.question().toLowerCase();
+
+} while (answer === 'yes');
+
+prompt(languageMessages.thankYou);
